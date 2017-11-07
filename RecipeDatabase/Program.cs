bd75a26bf789c91d9d7877557f2dd4f5a37c54f9
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-11-07 11:22:57 +0100

    Add project files.

---
+++ RecipeDatabase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeDatabase
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SQLiteConnection cn = new SQLiteConnection(@"Data Source=C:\Users\Studerande\Downloads\sqlitestudio-3.1.1\SQLiteStudio\Recept.db");
+			cn.Open();
+			var cmd = cn.CreateCommand();
+			cmd.CommandText = "SELECT recipe_name FROM Maträtter";
+			var reader = cmd.ExecuteReader();
+			while (reader.Read())
+			{
+				Console.WriteLine(reader.GetString(0));
+			}
+			cn.Close();
+			Console.ReadKey();
+		}
+	}
+}
